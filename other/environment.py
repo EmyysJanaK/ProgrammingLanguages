a669818bbd32308fef5b132e65633b62369e4f98
JanakCSE210598B <janaksenevirathne8@gmail.com>
2024-08-26 00:54:50 +0530

    Add Control Structure and Scanner

--- other/environment.py
+++
@@ -1,5 +0,0 @@
-class Environment:
-    def __init__(self):
-        self.prev = None  # Pointer to the previous environment
-        self.name = "env0"  # Default name
-        self.boundVar = {}  # Map of bound variables to their values
